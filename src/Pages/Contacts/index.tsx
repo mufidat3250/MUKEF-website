51c46f05e123c0a495d8016dfa36047e862e9eb8
mufidah_cv <mufidah@codevillage.ng>
2022-08-26 10:06:18 +0000

    fixes

--- src/Pages/Contacts/index.tsx
+++ src/Pages/Contacts/index.tsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const Contacts = () => {
   return (
-    <div>Contacts</div>
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Doloremque ducimus tempora aut sapiente eos est laboriosam rerum debitis laudantium officiis, et id fugiat dolore, architecto magnam quasi eaque beatae possimus.</p>
   )
 }
 
